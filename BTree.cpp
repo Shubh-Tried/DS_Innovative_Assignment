f5d8dcab60838ef89b927f317dce34f18407e19a
Meer30 <meerptl2922@gmial.com>
2026-04-01 21:35:16 +0530

    upadated split function as per requirement.

--- BTree.cpp
+++ BTree.cpp
@@ -2,6 +2,7 @@
 #include <cstddef>
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 #include "BTree.h"
 
@@ -18,46 +19,79 @@ BTree::EachNode::EachNode(vector<Tick> *t, EachNode *l, EachNode *m) {
   this->d2 = nullptr;
 }
 
-bool BTree::split(EachNode **en, EachNode **n1) {
-  EachNode *n2 = new EachNode((*en)->d1, (*en)->l);
-  EachNode *n3 = new EachNode((*en)->d2, (*en)->m, (*en)->r);
-  // EachNode *l = (*en)->l;
-  // EachNode *m = (*en)->m;
-  if ((*en)->d2 == nullptr || (*(*en)->d2).empty()) {
+// bool BTree::split(EachNode **en, EachNode **n1) {
+//   EachNode *n2 = new EachNode((*en)->d1, (*en)->l);
+//   EachNode *n3 = new EachNode((*en)->d2, (*en)->m, (*en)->r);
+//   // EachNode *l = (*en)->l;
+//   // EachNode *m = (*en)->m;
+//   if ((*en)->d2 == nullptr || (*(*en)->d2).empty()) {
 
-    if ((*(*n1)->d1)[0].symbol > (*(*en)->d1)[0].symbol) {
-      (*n1)->d2 = (*en)->d1;
-      (*n1)->r = (*en)->m;
-      delete (*en);
-      *en = *n1;
-    } else {
-      (*en)->d2 = (*n1)->d1;
-      (*en)->r = (*n1)->m;
-      (*en)->m = (*n1)->l;
-    }
-    return false;
-  }
-  delete (*en);
-  if ((*(*n1)->d1)[0].symbol > (*n2->d1)[0].symbol) {
-    if ((*(*n1)->d1)[0].symbol > (*n3->d1)[0].symbol) {
-      n3->l = n2;
-      n3->m = *n1;
+//     if ((*(*n1)->d1)[0].symbol > (*(*en)->d1)[0].symbol) {
+//       (*n1)->d2 = (*en)->d1;
+//       (*n1)->r = (*en)->m;
+//       delete (*en);
+//       *en = *n1;
+//     } else {
+//       (*en)->d2 = (*n1)->d1;
+//       (*en)->r = (*n1)->m;
+//       (*en)->m = (*n1)->l;
+//     }
+//     return false;
+//   }
+//   delete (*en);
+//   if ((*(*n1)->d1)[0].symbol > (*n2->d1)[0].symbol) {
+//     if ((*(*n1)->d1)[0].symbol > (*n3->d1)[0].symbol) {
+//       n3->l = n2;
+//       n3->m = *n1;
 
-      *en = n3;
-    } else {
-      n2->m = (*n1)->l;
-      n3->l = (*n1)->l;
-      (*n1)->l = n2;
-      (*n1)->m = n3;
-      *en = *n1;
-    }
-  } else {
-    n2->l = *n1;
-    n2->m = n3;
-    *en = n2;
-  }
-  return true;
+//       *en = n3;
+//     } else {
+//       n2->m = (*n1)->l;
+//       n3->l = (*n1)->l;
+//       (*n1)->l = n2;
+//       (*n1)->m = n3;
+//       *en = *n1;
+//     }
+//   } else {
+//     n2->l = *n1;
+//     n2->m = n3;
+//     *en = n2;
+//   }
+//   return true;
+// }
+
+bool BTree::split(EachNode **en,EachNode **n1){
+
+  vector<index*> keys;
+  keys.push_back((*en)->d1);
+
+  if((*en)->d2 != nullptr)
+     keys.push_back((*en)->d2);
+  
+  keys.push_back((*n1)->d1);
+
+  sort(keys.begin(), keys.end(), [](index* a, index* b){
+     return a->key < b->key;
+  });
+  
+  index* lkey = keys[0];
+  index* mkey = keys[1];
+  index* rkey = keys[2];
+
+  EachNode *lnode = new EachNode(lkey);
+  EachNode *rnode = new EachNode(rkey);
+
+  (*en)->d1 = mkey;
+  (*en)->d2 = nullptr;
+
+  (*en)->l = lnode;
+  (*en)->m = rnode;
+  (*en)->r = nullptr;
+
+  return true;  
+
 }
+
 
 bool BTree::insertEle(EachNode **en, vector<Tick> *t) {
   if (en == nullptr) {
